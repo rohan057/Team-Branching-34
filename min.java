09a4dedbd161b8d1e696506c4ab5b3da51e59178
Y-e-x <sats222@outlook.com>
2022-10-27 10:23:28 +0100

    minimum class

---
+++ min.java
@@ -0,0 +1,24 @@
+package tp2;
+
+import java.util.Scanner;
+
+public class min {
+Scanner input= new Scanner(System.in);
+System.out.println("Enter Number 1");
+
+int num1 = input.nextInt();  // Read user input1
+System.out.println("Enter Number 2");
+int num2=input.nextInt();
+this.num1=num1;
+this.num2=num2;}
+}
+
+ int minNum(){
+	 if (num1<= num2) {
+	 return num1;}
+	 else {
+		 return num2;
+	 }
+ }
+ }
+
